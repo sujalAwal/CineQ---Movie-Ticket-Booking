e2531f96b4449f6687b7a16851def8926fa1b490
sujal awal <sujal.awal@amniltech.com>
2025-10-01 10:48:20 +0545

    #initial Setup

---
+++ src/main/java/com/awal/cineq/CineqApplication.java
@@ -0,0 +1,13 @@
+package com.awal.cineq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/awal/cineq/CineqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awal.cineq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
